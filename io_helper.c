8dbf6aa354a6e7bda86d53fba09b3c572df2fa6f
Ben <benedict.boyle@protonmail.com>
2019-06-23 13:09:13 +0100

    Included data preparation routines for ECB mode

--- io_helper.c
+++ io_helper.c
@@ -42,33 +42,112 @@ void free_indata(indata)
 
 data16 prepare_data16(indata input)
 {
-	/* Take input buffer, return pointer to list of blocks PKCS #7 padded to appropriate length
+	/* Take input buffer, return pointer to list of blocks PKCS #7 padded to appropriate length for ECB mode
 	 */
-	data16 ret;
-	if (input.inlen % 4 == 0 || input.inlen % 4 == 1) 
-		ret.len = input.inlen/2 + 2;
-	else 
-		ret.len = input.inlen/2 + 1;
 
+	size_t pad = ((size_t) 4) - (input.inlen % 4);
+	indata p_input;
+	p_input.inlen = input.inlen + pad;
+	p_input.inbuf = realloc(input.inbuf, p_input.inlen);
+	if (p_input.inbuf == NULL) {
+		fprintf(stderr, "Unable to allocate memory for padding input in data preparation routine. Terminating.\n");
+		exit(1); //Think of better error handling scheme
+	}
+
+	size_t i;
+	for (i = input.inlen; i < p_input.inlen; i++)
+		* (p_input.inbuf + i) = (uint8_t) pad;
+
+	data16 ret;
+	ret.len = p_input.inlen/2;
 	ret.text = malloc(ret.len*sizeof(uint16_t));
 	if (ret.text == NULL) {
 		fprintf(stderr, "Unable to allocate memory in data preparation routine. Terminating.\n");
 		return ret; //make sure to handle error in main
 	}
 
+	size_t i;
+
+	for (i = 0; i < ret.len; i++)
+		* (ret.text + i) = ((* (p_input.inbuf + 2*i)) << 8) | (* (p_input.inbuf + 2*i + 1));
+
 	return ret;
 }
 
 data32 prepare_data32(indata input)
 {
-	data32 ret;
+	/* Take input buffer, return pointer to list of blocks PKCS #7 padded to appropriate length for ECB mode
+	 */
+
+	size_t pad = ((size_t) 8) - (input.inlen % 8);
+	indata p_input;
+	p_input.inlen = input.inlen + pad;
+	p_input.inbuf = realloc(input.inbuf, p_input.inlen);
+	if (p_input.inbuf == NULL) {
+		fprintf(stderr, "Unable to allocate memory for padding input in data preparation routine. Terminating.\n");
+		exit(1); //Think of better error handling scheme
+	}
+
+	size_t i;
+	for (i = input.inlen; i < p_input.inlen; i++)
+		* (p_input.inbuf + i) = (uint8_t) pad;
+
+	data16 ret;
+	ret.len = p_input.inlen/4;
+	ret.text = malloc(ret.len*sizeof(uint32_t));
+	if (ret.text == NULL) {
+		fprintf(stderr, "Unable to allocate memory in data preparation routine. Terminating.\n");
+		return ret; //make sure to handle error in main
+	}
+
+	size_t i;
+
+	for (i = 0; i < ret.len; i++) 
+		* (ret.text + i) = ((* (p_input.inbuf + 4*i)) << 24) |\
+				   * ((* (p_input.inbuf + 4*i + 1)) << 16) | \
+				   * ((* (p_input.inbuf + 4*i + 2)) << 8) | \
+				   * ((* (p_input.inbuf + 4*i + 3)));
 
 	return ret;
 }
 
 data64 prepare_data64(indata input)
 {
-	data64 ret;
+	/* Take input buffer, return pointer to list of blocks PKCS #7 padded to appropriate length for ECB mode
+	 */
+
+	size_t pad = ((size_t) 16) - (input.inlen % 16);
+	indata p_input;
+	p_input.inlen = input.inlen + pad;
+	p_input.inbuf = realloc(input.inbuf, p_input.inlen);
+	if (p_input.inbuf == NULL) {
+		fprintf(stderr, "Unable to allocate memory for padding input in data preparation routine. Terminating.\n");
+		exit(1); //Think of better error handling scheme
+	}
+
+	size_t i;
+	for (i = input.inlen; i < p_input.inlen; i++)
+		* (p_input.inbuf + i) = (uint8_t) pad;
+
+	data16 ret;
+	ret.len = p_input.inlen/4;
+	ret.text = malloc(ret.len*sizeof(uint64));
+	if (ret.text == NULL) {
+		fprintf(stderr, "Unable to allocate memory in data preparation routine. Terminating.\n");
+		return ret; //make sure to handle error in main
+	}
+
+	size_t i;
+
+	for (i = 0; i < ret.len; i++) 
+		* (ret.text + i) = ((* (p_input.inbuf + 8*i)) << 56) |\
+				   * ((* (p_input.inbuf + 8*i + 1)) << 48) | \
+				   * ((* (p_input.inbuf + 8*i + 2)) << 40) | \
+				   * ((* (p_input.inbuf + 8*i + 3)) << 32) | \
+				   * ((* (p_input.inbuf + 8*i + 4)) << 24) | \
+				   * ((* (p_input.inbuf + 8*i + 5)) << 16) | \
+				   * ((* (p_input.inbuf + 8*i + 6)) << 8) | \
+				   * ((* (p_input.inbuf + 8*i + 7)));
 
 	return ret;
 }
